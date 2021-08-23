38fece7d286858c8ba6328b2a00fb983612dc69c
rounaklama <rounaklamarl4@gmail.com>
2021-08-23 15:05:52 +0530

    Initial Commit

---
+++ vector coercion and vector naming.R
@@ -0,0 +1,35 @@
+vector coercion. and vector naming.
+x= c("s", 2)
+y=c(T,3)
+n=c("a", T)
+class(n,x,y)
+class(n)
+b=(5L,'a')
+b=c(5L,'a')
+as.character(5,"abc")
+as.character(x)
+as.character(y)
+as.integer(n)
+
+
+
+naming
+names(b)
+  b=letters[1:2]
+names(b)  
+b
+names(b)=letters[1:2]
+b
+names(b)
+names(n)=letters[1:2]
+n
+names(x)=letters[1:3]
+length(n)
+names(y)=LETTERS[1:2]
+b
+names(n)=LETTERS[1:2]
+Bb
+b
+names(b)
+b=c("d"=4,"e"=5,"f"=6,"g"=7,"h"=8)
+names(b)
